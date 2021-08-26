a18664793747f59add9edb429711bf67323787e5
Kiefer Levine <kiefer_8@hotmail.com>
2021-08-26 13:53:41 -0400

    Added contact form to page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/ContactForm';
 
 function App() {
   const [categories] = useState([
@@ -32,6 +33,7 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery
           currentCategory={currentCategory}
         ></Gallery>
